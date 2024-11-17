0ac8d38522aa0dfe515561629d3623464f79a0e2
PeerapongMala <151112918+PeerapongMala@users.noreply.github.com>
2024-11-17 22:41:13 +0700

    หห

--- src/main/java/ac/th/fearfreeanimals/controller/AdminController.java
+++ src/main/java/ac/th/fearfreeanimals/controller/AdminController.java
@@ -5,17 +5,20 @@ import ac.th.fearfreeanimals.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ac.th.fearfreeanimals.dto.UserRequest;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+
     @Autowired
     private Adminservice adminService;
 
     @PostMapping("/add-doctor")
-    public ResponseEntity<User> addDoctor(@RequestParam String username, @RequestParam String password) {
-        User doctor = adminService.createDoctor(username, password);
+    public ResponseEntity<User> addDoctor(@RequestBody UserRequest userRequest) {
+        User doctor = adminService.createDoctor(userRequest.getUsername(), userRequest.getPassword());
         return ResponseEntity.ok(doctor);
     }
 }
+
 
